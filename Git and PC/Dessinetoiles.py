8ae95b6cd83103214a0af8ace2ce1beddc6c7b64
Joseph-dev1948 <josephratajczykcathala@gmail.com>
2026-01-28 18:37:58 +0100

    modified:   Git and PC/Dessinetoiles.py 	modified:   Git and PC/With Claude/PNG to ASCII.py

--- Git and PC/Dessinetoiles.py
+++ Git and PC/Dessinetoiles.py
@@ -15,6 +15,7 @@ Ou un rectangle ---------------> (7)
 Ou un rectangle avec un trou --> (8)
 Ou un cercle ------------------> (9)
 Ou un cercle avec un trou -----> (10)
+Ou une image en ASCII ---------> (11)
 Ou 'stop' pour fermer le programme? : """)
         if choix == "1":
             pyramide()
@@ -45,6 +46,9 @@ Ou 'stop' pour fermer le programme? : """)
             break
         elif choix == "10":
             cercle_trou()
+            break
+        elif choix == "11":
+            image_ascii()
             break
         elif choix == "stop":
             break
@@ -158,6 +162,36 @@ def cercle_trou():
             else:
                 ligne += " "
         print(ligne)
+    menu()
+
+def image_ascii():
+    def image_to_ascii(width=100):
+        image_path = input("Chemin de l'image (ex: ./img_for_ascii/tête_de_mort.png) : ")
+        img = Image.open(image_path)
+        
+        img = img.convert('L')
+        
+        aspect_ratio = img.height / img.width
+        new_height = int(width * aspect_ratio * 0.55)  # 0.55 pour compenser la hauteur des caractères
+        img = img.resize((width, new_height))
+        
+        
+        pixels = img.getdata()
+        ascii_str = ""
+        
+        for i, pixel in enumerate(pixels):
+            if pixel < 128:  # Sombre = étoile changer le < par > pour inverser les couleurs
+                ascii_str += "*"
+            else:  # Clair = espace
+                ascii_str += " "
+            
+            if (i + 1) % width == 0:
+                ascii_str += "\n"
+        
+        return ascii_str
+    
+    art = image_to_ascii(width=80)
+    print(art)
     menu()
     
 menu()
--- Git and PC/With Claude/PNG to ASCII.py
+++ Git and PC/With Claude/PNG to ASCII.py
@@ -1,29 +1,30 @@
 from PIL import Image
 
-def image_to_ascii(image_path, width=100):
-    
-    img = Image.open(image_path)
-    
-    img = img.convert('L')
-    
-    aspect_ratio = img.height / img.width
-    new_height = int(width * aspect_ratio * 0.55)  # 0.55 pour compenser la hauteur des caractères
-    img = img.resize((width, new_height))
-    
-    
-    pixels = img.getdata()
-    ascii_str = ""
-    
-    for i, pixel in enumerate(pixels):
-        if pixel < 128:  # Sombre = étoile changer le < par > pour inverser les couleurs
-            ascii_str += "*"
-        else:  # Clair = espace
-            ascii_str += " "
+def image_ascii():
+    def image_to_ascii(width=100):
+        image_path = input("Chemin de l'image (ex: ./img_for_ascii/tête_de_mort.png) : ")
+        img = Image.open(image_path)
         
-        if (i + 1) % width == 0:
-            ascii_str += "\n"
+        img = img.convert('L')
+        
+        aspect_ratio = img.height / img.width
+        new_height = int(width * aspect_ratio * 0.55)  # 0.55 pour compenser la hauteur des caractères
+        img = img.resize((width, new_height))
+        
+        
+        pixels = img.getdata()
+        ascii_str = ""
+        
+        for i, pixel in enumerate(pixels):
+            if pixel < 128:  # Sombre = étoile changer le < par > pour inverser les couleurs
+                ascii_str += "*"
+            else:  # Clair = espace
+                ascii_str += " "
+            
+            if (i + 1) % width == 0:
+                ascii_str += "\n"
+        
+        return ascii_str
     
-    return ascii_str
-
-art = image_to_ascii("./img_for_ascii/tête_de_mort.png", width=80)
-print(art)
+    art = image_to_ascii(width=80)
+    print(art)
