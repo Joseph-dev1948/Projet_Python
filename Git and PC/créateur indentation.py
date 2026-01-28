d0c8d95ab20068a131e77d0420e4ad42390e09ec
Joseph-dev1948 <josephratajczykcathala@gmail.com>
2026-01-28 08:24:16 +0100

    Delete Git and PC directory

--- Git and PC/créateur indentation.py
+++
@@ -1,8 +0,0 @@
-alphabet = ["b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
-voiture = 4
-bip = 8
-print(f'for a in range({int(bip)}):')
-for x in alphabet[:26]:
-    print(" "*int(voiture),f'for {x} in range({int(bip)}):')
-    voiture +=4
-print(" "*int(voiture),f'print("caca")')
--- Git and PC/programme de sport.py
+++
@@ -1,7 +0,0 @@
-from random import*
-
-exercice = ["pompes","tractions","squats","gainage","dips","relever de jambes sur barre","handstand hold"]
-
-for exercice in exercice:
-    nombre = randint(1,20)
-    print(f"{exercice} : {nombre}")
